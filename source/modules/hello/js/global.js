57de6a8e9b8a96184d6b56861357238b7c05dc23
Cezary Sliwiński <csliwinski@squiz.pl>
2016-11-29 14:45:06 +0100

    New html and css structure of starting banner

--- source/modules/hello/js/global.js
+++ source/modules/hello/js/global.js
@@ -1,10 +1,8 @@
 var bannerHeight = {
     init: function () {
         'use strict';
-        var windowheight = $(window).height() - $('.b-header').height();
-        var headlineheight = windowheight - $('.b-banner__treatments').height() - $('.b-banner__clinics').height() - 80;
+        var windowheight = $(window).height() - $('.b-layout__header').height();
         $('.b-banner').height(windowheight);
-        $('.b-banner__headline').height(headlineheight);
     }
 };
 
